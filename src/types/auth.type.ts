af273dad210f24144facee74dc763eaf54848668
Deekshaagg05 <daggarwal50_be23@thapar.edu>
2026-06-11 20:38:32 +0530

    login update

---
+++ src/types/auth.type.ts
@@ -0,0 +1,24 @@
+export interface LoginFormValues {
+  user_unique_id: string;
+  user_password: string;
+  remember: boolean;
+}
+
+export interface ForgotPasswordFormValues {
+  user_unique_id: string;
+}
+
+export interface LoginApiResponse {
+  sessionId: string;
+}
+
+export interface UserSummary {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+export interface SummaryApiResponse {
+  userSummary: UserSummary;
+}
